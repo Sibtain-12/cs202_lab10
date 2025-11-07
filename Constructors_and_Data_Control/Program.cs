633026a4aab345c6b17a5ced0d0baf8141845da1
Md Sibtain Raza <mdsibtain.raza@iitgn.ac.in>
2025-11-08 03:12:04 +0530

    done

---
+++ Constructors_and_Data_Control/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Lab10.Constructors
+{
+    class Program
+    {
+        private int data;
+        private static int liveCount = 0;
+
+        public Program()
+        {
+            liveCount++;
+            Console.WriteLine($"Constructor Called. Active objects: {liveCount}");
+        }
+
+        ~Program()
+        {
+            liveCount--;
+            Console.WriteLine($"Object Destroyed. Active objects: {liveCount}");
+        }
+
+        public void set_data(int x) => data = x;
+
+        public void show_data() => Console.WriteLine($"data = {data}");
+
+        // Create objects in a separate scope so they become unreachable
+        static void MakeObjects()
+        {
+            var p1 = new Program();
+            var p2 = new Program();
+            var p3 = new Program();
+
+            p1.set_data(10);
+            p2.set_data(20);
+            p3.set_data(30);
+            Console.WriteLine("\nValues in sequential order:");
+            p1.show_data();
+            p2.show_data();
+            p3.show_data();
+            Console.WriteLine();
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Creating objects...\n");
+            MakeObjects();                    
+            GC.Collect();                     
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            Console.WriteLine();          
+            Console.WriteLine("End of Main");
+        }
+    }
+}
---
+++ Inheritance_and_Method_Overriding/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Lab10.Inheritance
+{
+    class Vehicle
+    {
+        protected int speed;
+        protected int fuel;
+
+        public Vehicle(int speed, int fuel)
+        {
+            this.speed = speed;
+            this.fuel = fuel;
+        }
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Vehicle Info -> Speed: {speed}, Fuel: {fuel}");
+        }
+
+        public virtual void Drive()
+        {
+            fuel -= 5;
+            Console.WriteLine("Vehicle is moving...");
+        }
+    }
+
+    class Car : Vehicle
+    {
+        private int passengers;
+
+        public Car(int speed, int fuel, int passengers) : base(speed, fuel)
+        {
+            this.passengers = passengers;
+        }
+
+        public override void Drive()
+        {
+            fuel -= 10;
+            Console.WriteLine("Car is moving with passenger");
+        }
+
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Car Info -> Speed: {speed}, Fuel: {fuel}, Passengers: {passengers}");
+        }
+    }
+
+    class Truck : Vehicle
+    {
+        private int cargoWeight;
+
+        public Truck(int speed, int fuel, int cargoWeight) : base(speed, fuel)
+        {
+            this.cargoWeight = cargoWeight;
+        }
+
+        public override void Drive()
+        {
+            fuel -= 15;
+            Console.WriteLine("Truck is moving with cargo");
+        }
+
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Truck Info -> Speed: {speed}, Fuel: {fuel}, CargoWeight: {cargoWeight}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Start of program\n");
+            Vehicle[] fleet = new Vehicle[]
+            {
+                new Vehicle(40, 100),
+                new Car(60, 80, 4),
+                new Truck(50, 120, 2000)
+            };
+
+            foreach (var v in fleet)
+            {
+                v.Drive();
+                v.ShowInfo();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("End of program");
+        }
+    }
+}
